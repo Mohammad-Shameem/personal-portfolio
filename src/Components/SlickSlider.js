2e2ac5641d5b04bed4c312e602d5579ede7dc005
unknown <mehmetsaki789@gmail.com>
2022-12-10 18:14:07 +0600

    responsive slick slider

--- src/Components/SlickSlider.js
+++ src/Components/SlickSlider.js
@@ -24,6 +24,32 @@ const SlickSlider = () => {
     pauseOnHover: false,
     pauseOnFocus: false,
     easing: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <div className="slick-carousel">
